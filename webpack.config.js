ab4f6ad8f289e2d7432d4788f9bb267cbce92a59
Полина Ульянова <polina.ulianova@urfu.me>
2025-04-06 23:19:00 +0500

    Разделяй и властвуй

--- webpack.config.js
+++ webpack.config.js
@@ -1,42 +1,58 @@
 const path = require('path');
-const CopyPlugin = require('copy-webpack-plugin');
-const HtmlPlugin = require('html-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   entry: './src/main.js',
+
   output: {
+    clean: {
+      keep: /\ignored\/dir\//
+    },
     filename: 'bundle.[contenthash].js',
     path: path.resolve(__dirname, 'build'),
-    clean: true,
   },
-  devtool: 'source-map',
-  plugins: [
-    new HtmlPlugin({
-      template: 'public/index.html',
-    }),
-    new CopyPlugin({
-      patterns: [
-        {
-          from: 'public',
-          globOptions: {
-            ignore: ['**/index.html'],
-          },
-        },
-      ],
-    }),
-  ],
+
+  devtool: 'inline-source-map',
+
   module: {
     rules: [
       {
-        test: /\.js$/,
-        exclude: /(node_modules)/,
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
         use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env']
-          },
-        },
+          loader: "babel-loader"
+        }
       },
     ],
   },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, `../src/templates`),
+          to: path.resolve(__dirname, `../build/templates`),
+        }
+      ],
+    }),
+  ],
+
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist'),
+    },
+    open: true,
+  },
+
+  mode: 'development',
 };
